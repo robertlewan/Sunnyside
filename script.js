b993df475753f2a455854aa8920835ba3652d1ca
robertlewan <anhtri.ngo99@gmail.com>
2021-09-02 13:58:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const toggleButton = document.getElementById('toggle');
 const navbarLink = document.getElementById('hidden-navbar');
-
-console.log(toggleButton);
-
 toggleButton.addEventListener('click', () => {
     navbarLink.classList.toggle('active');
 })
